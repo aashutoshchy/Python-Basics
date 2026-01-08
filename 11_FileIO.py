741389fdb2802bb24cce14a2268eb7062b23ec47
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-01-08 23:39:54 +0545

    practice

---
+++ 11_FileIO.py
@@ -0,0 +1,38 @@
+# File I/O in Python
+# Opening and Reading File
+# f = open("file.txt", "r")
+# data1 = f.readline()
+# print(data1)
+
+# data2 = f.readline()
+# print(data2)
+# f.close()
+
+
+# Writing to a file
+# f = open("file.txt", "w")
+
+# f.write("This is new text")
+
+# To Append New text
+
+# f = open("file.txt", "a")
+
+# f.write("\nThis text is appended")
+
+
+# with Syntax 
+# It automatically close file at the end
+# with open("file.txt", "r") as  f:
+
+#     data = f.read()
+#     print(data)
+
+# with open("file.txt", "w") as f:
+#     f.write("This is new data with with Syntax")
+
+# Deleting a file
+# import os
+# os.remove("sample.txt")
+
+# Practice Question 1
---
+++ Practice/one.py
@@ -0,0 +1,8 @@
+sum, current = 0, 1
+n = 10
+# while current <= n:
+sum = sum + current
+current = current + 1
+
+print(sum)
+print(current)
