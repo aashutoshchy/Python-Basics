9d93689361166b88ae72bfc815a6bb44e9a3f400
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-01-09 12:03:20 +0545

    50/50

---
+++ Practice/two.py
@@ -0,0 +1,19 @@
+# fruit_prices = {
+# 'apples': '.66', 'pears': '.25', 'peaches': '.74', 'bananas': '.49'
+# }
+
+# print(fruit_prices['apples'])
+
+# veg_data = [['corn', '.25'], ['tomatoes', '.49'], ['peas', '.39']]
+# veg_prices = dict(veg_data)
+# print(veg_prices)
+# print( veg_prices['peas'])
+
+# colours = {'spade': 'red', 'bucket': 'blue', 'towel': 'grey'}
+# for key in colours.keys():
+#     print(key)
+
+
+colours = {'spade': 'red', 'bucket': 'blue', 'towel': 'grey'}
+for key, value in colours.items():
+    print(key, value)
